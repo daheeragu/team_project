ddc0c1c9db478746871e78a9e9f3f746d5a78736
jeonginGIM <kji03060758@gmail.com>
2022-05-31 12:10:30 +0900

    ckeditor 병합충돌 해결

--- src/main/webapp/resources/ckeditor/config.js
+++ src/main/webapp/resources/ckeditor/config.js
@@ -7,11 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-<<<<<<< HEAD
-=======
 	
 	 config.extraPlugins = 'image2';
->>>>>>> 9685be7ecbf97bbe2ca53261c0167e319ec038a6
-};
 
-config.extraPlugin='image2';
+};
